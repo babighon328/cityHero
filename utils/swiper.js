a9a874ac5e59a79799ab27182842a862425efd74
ivang_v (Ivan Golubev) <i.golubev@rosalinux.ru>
2025-01-30 00:09:38 +0300

    fix

--- utils/swiper.js
+++ utils/swiper.js
@@ -1,27 +1,23 @@
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3, 
+  spaceBetween: 0, 
   loop: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
   },
   autoplay: {
     delay: 3000,
   },
 });
 
-const swiper1 = new Swiper('.swiper-container1', {
+const swiper1 = new Swiper('.swiper1', {
+  slidesPerView: 3, 
+  spaceBetween: 0, 
   loop: true,
   pagination: {
     el: '.swiper-pagination1',
     clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next1',
-    prevEl: '.swiper-button-prev1',
   },
   autoplay: {
     delay: 3000,
